464af40da9454d38e692e582d86fdad5b35b3fc5
oswaldo-galeana <yoed.dev@gmail.com>
2026-06-09 23:46:43 -0600

    chore:config de tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-center mt-10 bg">
+      Hola tailwind 
+    </h1>
+  )
+}
+
+export default App

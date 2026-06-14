eaf06eda2858954b6f64c92b6e5de23fd71c7f6b
oswaldo-galeana <yoed.dev@gmail.com>
2026-06-14 01:18:44 -0600

    feat: ProductCard agregado

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,19 @@
+export default function ProductCard({imagen, categoria, nombre, descripcion, precio}) {
+    return (
+        <div className="flex flex-col rounded-xl border border-gold-main bg-cream-surface overflow-hidden">
+            <img src= {imagen} alt={nombre} className="w-full aspect-square object-cover"/>
+              <div className="flex flex-col px-3 py-2">
+            <p className="font-body text-olive-base text-xs font-semibold ">{categoria}</p>
+            <p className="font-display text-lg text-text-primary py-2">{nombre}</p>
+            <p className="font-body text-sm text-text-secondary pb-2">{descripcion}</p>
+
+            <div className="flex flex-row gap-2 items-center justify-between mt-3">
+            <p className="font-body text-gold-main text-xl italic">{precio}</p>
+            <Button variant="primary">
+            <FontAwesomeIcon icon={faWhatsapp} size="lg"/> Pedir</Button>
+            </div>
+            
+        </div>
+        </div>
+    )
+}
